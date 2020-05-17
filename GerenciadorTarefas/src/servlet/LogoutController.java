941f2fa36b29b1e6c42ac1fbc8de823d85166823
nadinyrajab <44749894+nadinyrajab@users.noreply.github.com>
2020-05-17 10:50:41 -0300

    Servlets

---
+++ GerenciadorTarefas/src/servlet/LogoutController.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/logout.do")
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sessao = request.getSession(); 
+		
+		sessao.removeAttribute("email");
+		sessao.removeAttribute("senha");
+		sessao.removeAttribute("nome");
+		sessao.invalidate();
+		response.sendRedirect("index.jsp");
+	
+	}
+
+}
